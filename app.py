8ded9161b4b0c12b2dbc7753827a543df0651ac9
Eduardo Huerta <90126777+EduardoVH@users.noreply.github.com>
2024-05-14 11:53:39 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -3,40 +3,51 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-reserved_words = {'for', 'do', 'while', 'if', 'int', 'else', 'print', 'end', 'read'}
-symbols = {';', '"', '+', '-', '=', ',', '(', ')', '{', '}'}
+reserved_words = {'for', 'do', 'while', 'if', 'int', 'else', 'print', 'end', 'read', 'programa'}
+symbols = {';', '"', '+', '=', ',', '(', ')', '{', '}'}
 
 def analyze_code(code):
     lines = code.split('\n')
     tokens = []
+    errors = []
 
     for i, line in enumerate(lines, start=1):
-        # Utilizar expresiones regulares para encontrar palabras y símbolos
         words = re.findall(r'\b\w+\b|[\(\){};"+=,]', line)
         for word in words:
+            if word in reserved_words:
+                if word not in reserved_words:
+                    errors.append({'token': word, 'line': i})
+                reserved = 'x'
+            else:
+                reserved = ''
             token = {
                 'token': word,
-                'linea': i,
-                'reservada': 'x' if word in reserved_words else '',
-                'simbolo': 'x' if word in symbols else '',
-                'parentesis_izquierdo': 'x' if word == '(' else '',
-                'parentesis_derecho': 'x' if word == ')' else '',
-                'llave_izquierda': 'x' if word == '{' else '',
-                'llave_derecha': 'x' if word == '}' else '',
-                'numero': 'x' if word.isdigit() else '',
-                'identificador': 'x' if word not in reserved_words and word not in symbols and not word.isdigit() else ''
+                'line': i,
+                'reserved': reserved,
+                'symbol': 'x' if word in symbols else '',
+                'comma': 'x' if word == ',' else '',
+                'semicolon': 'x' if word == ';' else '',
+                'left_paren': 'x' if word == '(' else '',
+                'right_paren': 'x' if word == ')' else '',
+                'left_brace': 'x' if word == '{' else '',
+                'right_brace': 'x' if word == '}' else '',
+                'number': 'x' if word.isdigit() else '',
+                'identifier': 'x' if word not in reserved_words and word not in symbols and not word.isdigit() else ''
             }
             tokens.append(token)
 
-    return tokens
+    total_counts = {key: sum(1 for token in tokens if token[key] == 'x') for key in tokens[0].keys() if key != 'token'}
+    tokens.append(total_counts)
+
+    return tokens, errors
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         code = request.form['code']
-        tokens = analyze_code(code)
-        return render_template('index.html', tokens=tokens, code=code)
-    return render_template('index.html', tokens=[], code='')
+        tokens, errors = analyze_code(code)
+        return render_template('index.html', tokens=tokens, code=code, errors=errors)
+    return render_template('index.html', tokens=[], code='', errors=[])
 
 if __name__ == '__main__':
     app.run(debug=True)
